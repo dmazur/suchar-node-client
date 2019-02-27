b32a945f3ac5ec50a906c287d65b7dc31ea6f74f
Dawid Mazur <dmazur.work@gmail.com>
2019-02-27 21:59:31 +0100

    I was wrong, adding CORS

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const randomSucharGenerator = require('random-suchar-generator/randomSucharGenerator');
 const app = express();
+const cors = require('cors');
 const port = 3000;
+
+app.use(cors());
 
 app.get('/', async (req, res) => {
     return randomSucharGenerator().then(
